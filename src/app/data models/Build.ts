e17834204e9320c0884545798037a5f9a4edab86
DrBaxR <alexflorea16000@gmail.com>
2020-12-26 12:47:37 +0200

    build-service: Generated the build service and created the data model for a build

---
+++ src/app/data models/Build.ts
@@ -0,0 +1,17 @@
+import { Case } from "./Case";
+import { CPU } from "./CPU";
+import { GPU } from "./GPU";
+import { Memory } from "./Memory";
+import { Motherboard } from "./Motherboard";
+import { PSU } from "./PSU";
+import { Storage } from "./Storage";
+
+export interface Build {
+    cpu: CPU;
+    motherboard: Motherboard;
+    memory: Memory;
+    storage: Storage[];
+    gpu: GPU;
+    _case: Case;
+    psu: PSU;
+}
